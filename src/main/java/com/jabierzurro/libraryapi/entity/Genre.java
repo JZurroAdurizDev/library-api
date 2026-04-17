169ba233943180a60aa34a441af89be580fef867
JZurroAdurizDev <jabier_zurro@hotmail.com>
2026-04-17 20:58:02 +0100

    feat: add Book and Genre entities

--- src/main/java/com/jabierzurro/libraryapi/entity/Genre.java
+++ src/main/java/com/jabierzurro/libraryapi/entity/Genre.java
@@ -6,8 +6,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.util.HashSet;
@@ -23,39 +21,22 @@ import lombok.Setter;
  * @author Jabier Zurro Aduriz
  */
 @Entity
-@Table(name = "books")
+@Table(name = "genres")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Book {
+public class Genre {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Integer id;
 
-    @Column(nullable = false, length = 200)
-    private String title;
+    @Column(name = "genre_name", nullable = false, length = 50, unique = true)
+    private String genreName;
 
-    @Column(nullable = false, length = 150)
-    private String author;
-
-    @Column(nullable = false, length = 13, unique = true)
-    private String isbn;
-
-    @Column(name = "published_year", nullable = false)
-    private Short publishedYear;
-
-    @Column
-    private Integer pages;
-
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(
-        name = "book_genres",
-        joinColumns = @JoinColumn(name = "book_id"),
-        inverseJoinColumns = @JoinColumn(name = "genre_id")
-    )
-    private Set<Genre> genres = new HashSet<>();
+    @ManyToMany(mappedBy = "genres", fetch = FetchType.LAZY)
+    private Set<Book> books = new HashSet<>();
 }
